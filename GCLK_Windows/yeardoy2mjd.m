d36e0fd7f16644f183fc3d70f71ae18e088d52e3
scf-lntu <songchuanfeng@lntu.edu.cn>
2024-06-28 10:47:43 +0800

    Delete GCLK_Windows directory

--- GCLK_Windows/yeardoy2mjd.m
+++
@@ -1,3 +0,0 @@
-function Imjd=yeardoy2mjd(Iyear,Idoy)
-
-Imjd=modified_julday(1,1,Iyear)+Idoy-1;
--- GCLK_Windows/yeardoy2wkd.m
+++
@@ -1,5 +0,0 @@
-function [nweek,nday]=yeardoy2wkd(iyear,idoy)
-
-mjd=yeardoy2mjd(iyear,idoy);
-nweek=floor((mjd-44244)/7);
-nday=mjd-44244-nweek*7;
